417240ebe45c9b2d65f9865752161870637b8b15
elizaveta-rybina <liza.rybina@inbox.ru>
2024-08-06 16:31:27 +0300

    add documentation

--- src/api/middlewares/GlobalErrorHandler.php
+++ src/api/middlewares/GlobalErrorHandler.php
@@ -2,19 +2,11 @@
 
 class GlobalErrorHandler
 {
-    /**
-     * Обработка исключений.
-     *
-     * Эта метод вызывается при возникновении исключения. Устанавливает код ответа HTTP 500
-     * и возвращает информацию об исключении в формате JSON.
-     *
-     * @param Throwable $exception Исключение, которое нужно обработать.
-     *
-     * @return void
-     */
     public static function handleException(Throwable $exception): void
     {
-        http_response_code(500);
+        if (!headers_sent()) {
+            http_response_code(500);
+        }
 
         echo json_encode([
             "code" => $exception->getCode(),
@@ -24,19 +16,6 @@ class GlobalErrorHandler
         ]);
     }
 
-    /**
-     * Обработка ошибок.
-     *
-     * Этот метод вызывается при возникновении ошибки в коде. Преобразует ошибку в исключение
-     * для дальнейшей обработки в методе `handleException`.
-     *
-     * @param int $errno Уровень ошибки.
-     * @param string $errstr Сообщение об ошибке.
-     * @param string $errfile Файл, в котором произошла ошибка.
-     * @param int $errline Номер строки, где произошла ошибка.
-     *
-     * @return bool Всегда возвращает false, чтобы PHP продолжал выполнение стандартной обработки ошибок.
-     */
     public static function handleError(
         int $errno,
         string $errstr,
